dd8339668ee7cb3eb44e75fa10e071945428fec4
Kacper <1a.kacper.pochopien@gmail.com>
2026-06-06 18:19:01 +0200

    allowed allow for every settings.py

--- streaming_service/settings.py
+++ streaming_service/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
