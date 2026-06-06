1d6759826cc2c30191d0e37d8812e52ac9025962
Kacper <1a.kacper.pochopien@gmail.com>
2026-06-06 18:27:55 +0200

    zmiana superuser

--- streaming_service/settings.py
+++ streaming_service/settings.py
@@ -1,20 +1,4 @@
 import os
-
-if os.environ.get('CREATE_SUPERUSER', 'False').lower() == 'true':
-    import django
-    django.setup()
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    username = os.environ.get('SUPERUSER_USERNAME', 'admin')
-    email = os.environ.get('SUPERUSER_EMAIL', 'admin@example.com')
-    password = os.environ.get('SUPERUSER_PASSWORD', 'Admin123!')
-    
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print(f'Superuser "{username}" created.')
-    else:
-        print(f'Superuser "{username}" already exists.')
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
